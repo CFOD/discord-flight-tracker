40ca11f87b24cf46aed74c3856e2120e5df7e7c9
Connor <connor-odonnell@hotmail.co.uk>
2026-05-23 16:51:20 +0100

    chore: force redeploy to bust Workers asset cache

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
+const _v = 2;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
