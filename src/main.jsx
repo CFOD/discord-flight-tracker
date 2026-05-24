7ebb282c829e654aa555de7d9eaf2442ca942f4b
Connor <connor-odonnell@hotmail.co.uk>
2026-05-24 01:01:11 +0100

    chore: bust asset cache

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
-const _v = 2;
+const _v = 3;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
